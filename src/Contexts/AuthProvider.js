13f4400f408731cc3a851464257bf03adcb166e8
EhsanEIK <ehsankhan1997.ek@gmail.com>
2024-11-26 11:57:31 +0600

    added handleSignup in SIgnup.js and added Loader component in project folder

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createContext } from 'react';
+import app from '../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    // sign up new user
+    const createNewUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // sign in
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // sign out
+    const logout = () => {
+        return signOut(auth);
+    }
+
+    // reset  password
+    const resetPassword = email => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    // get current sign in user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => unsubscribe();
+    }, [])
+
+    const authInfo = {
+        createNewUser,
+        login,
+        logout,
+        resetPassword,
+        user,
+        loading,
+        setLoading
+    };
+
     return (
-        <div>
-
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
